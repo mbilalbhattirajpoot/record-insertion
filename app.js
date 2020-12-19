14fddcba8ffe35a9c33d4337b195d864224b96d8
mbilalbhattirajpoot <m.bilalbhattirajpoot@gmail.com>
2020-12-19 12:41:48 -0800

    #3 nodemon

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-var express= require ('express')
+var express = require('express')
 var app = express();
 
-app.get('/name',(req,res,next)=>{
+app.get('/name', (req, res, next) => {
+    res.json({
+        message: "hi i am form get req"
+    })
     console.log('hi i am from name root')
 })
 
-app.listen(3000,()=>{
-console.log('hi your server started on 3000')
+app.post('/address',function(req,res){
+        res.json({
+            message:"hi i am from post req"
+        })
+        console.log('hi i am from address root')
+})
+
+app.listen(3000,function (req,res) {
+    console.log('hi your server started on 3000')
 
 
 })
